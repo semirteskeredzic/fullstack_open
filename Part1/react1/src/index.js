8abbb31a120984db65a3172c352a1846eb6dc0b4
Semir Teskeredzic <semir.teskeredzic@gmail.com>
2019-08-22 13:17:44 +0200

    Update index.js
    removed blank space

--- Part1/react1/src/index.js
+++ Part1/react1/src/index.js
@@ -15,7 +15,7 @@ const Part = (props) => {
 
 const Total = (props) => {
     return (
-        <p>Number of exercises is {props.exercises1 + props.exercises2 + props.exercises3} </p>
+        <p>Number of exercises is {props.exercises1 + props.exercises2 + props.exercises3}</p>
     )
 }
 
